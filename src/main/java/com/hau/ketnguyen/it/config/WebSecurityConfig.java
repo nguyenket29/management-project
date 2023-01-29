6e253865a5a00197e56f38380d01703d051ae979
nguyenket29 <ketmax121@gmail.com>
2023-01-29 15:13:01 +0700

    fix cors

--- src/main/java/com/hau/ketnguyen/it/config/WebSecurityConfig.java
+++ src/main/java/com/hau/ketnguyen/it/config/WebSecurityConfig.java
@@ -66,7 +66,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(ImmutableList.of("*"));
+        configuration.setAllowedOriginPatterns(ImmutableList.of("*"));
         configuration.setAllowedMethods(ImmutableList.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(ImmutableList.of("*"));
